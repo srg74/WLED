470b396854e80aa74c4a9d686deacb371aad6efc
srg74 <28492985+srg74@users.noreply.github.com>
2025-09-01 15:19:47 -0400

    Update wled.h

--- wled00/wled.h
+++ wled00/wled.h
@@ -7,7 +7,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2508310
+#define VERSION 2509010
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
